340e39001839387a10b4782864883d0174befe6a
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-20 15:43:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "ap-southeast-2"  # Change this to your desired region
 }
 
-//1. Create IAM Role and Policy for EC2 to push logs to CloudWatch
 resource "aws_iam_role" "ec2_instance_role" {
   name = "EC2CloudWatchRole"
 
@@ -18,19 +17,17 @@ resource "aws_iam_role" "ec2_instance_role" {
   })
 }
 
-# Attach the CloudWatch Logs full access policy to the role
 resource "aws_iam_role_policy_attachment" "attach_cw_logs_policy" {
   role       = aws_iam_role.ec2_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
-# 2. Create an EC2 instance profile
+
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2InstanceProfile"
   role = aws_iam_role.ec2_instance_role.name
 }
 
-# 3. Create a Security Group for EC2 instance
 resource "aws_security_group" "ec2_sg" {
   name        = "allow_ssh_http"
   description = "Allow SSH and HTTP inbound traffic"
@@ -58,7 +55,6 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-# 4. Create EC2 instance and install Docker
 resource "aws_instance" "docker_ec2" {
   ami           = "ami-084e237ffb23f8f97"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
@@ -92,7 +88,6 @@ resource "aws_instance" "docker_ec2" {
     }
     EOT
 
-    # Restart Docker service to apply the changes
     sudo service docker restart
   EOF
 
@@ -101,7 +96,6 @@ resource "aws_instance" "docker_ec2" {
   }
 }
 
-# 5. Create CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "docker_log_group" {
   name = "docker-container-logs"
   retention_in_days = 7  # Log retention (can adjust as needed)
