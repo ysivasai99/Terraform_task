b255c17b8fcb105f4a6acd9f2cda38859be73c4d
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-20 20:50:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,62 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "EC2CloudWatchRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_cw_logs_policy" {
+  role       = aws_iam_role.ec2_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "EC2InstanceProfile"
+  role = aws_iam_role.ec2_instance_role.name
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id  # Dynamically fetch the default VPC
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "docker_ec2" {
   ami           = "ami-084e237ffb23f8f97"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
@@ -8,16 +67,16 @@ resource "aws_instance" "docker_ec2" {
 
   user_data = <<-EOF
     #!/bin/bash
-    # Update the system and install Docker and Git
     sudo yum update -y
     sudo amazon-linux-extras install docker -y
-    sudo yum install git -y
     sudo service docker start
     sudo usermod -a -G docker ec2-user
 
     sudo yum install -y awslogs
     sudo systemctl start awslogsd
     sudo systemctl enable awslogsd.service
+
+    aws logs create-log-group --log-group-name /aws/docker/backend-logs --region ap-southeast-2 || true
 
     cat <<EOT > /etc/docker/daemon.json
     {
@@ -33,17 +92,20 @@ resource "aws_instance" "docker_ec2" {
 
     sudo service docker restart
 
-
     git clone https://github.com/agri-pass/agri-pass-backend.git /home/ec2-user/agri-pass-backend
 
     cd /home/ec2-user/agri-pass-backend
     docker build -t agri-pass-backend-image .
 
     docker run -d --name backend-container agri-pass-backend-image
-
   EOF
 
   tags = {
     Name = "DockerInstance"
   }
+}
+
+resource "aws_cloudwatch_log_group" "docker_log_group" {
+  name              = "/aws/docker/backend-logs"
+  retention_in_days = 7
 }
