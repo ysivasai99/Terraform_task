83296a7f2ad1281dfdcc15f5457793c730b4c255
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-22 19:22:50 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,27 +23,6 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_full_access" {
   role       = aws_iam_role.ec2_cloudwatch_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
-resource "aws_iam_role" "ec2_cloudwatch_role" {
-  name = "ec2_cloudwatch_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cloudwatch_full_access" {
-  role       = aws_iam_role.ec2_cloudwatch_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
-}
 
 
 resource "aws_instance" "ec2_instance" {
