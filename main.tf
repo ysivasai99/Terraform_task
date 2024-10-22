150d54c307299e3e6fe79c2bf2e796747d4df221
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-22 10:39:31 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,14 +30,14 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_logs_attachment" {
 }
 
 # IAM Instance Profile
-resource "aws_iam_instance_profile" "ec2_cloudwatch_logs_profile1" {
-  name = "EC2-CloudWatch-Logs-Instance-Profile-Static"
+resource "aws_iam_instance_profile" "ec2_cloudwatch_logs_profile71" {
+  name = "EC2-CloudWatch-Logs-Instance-Profile-Static1"
   role = aws_iam_role.ec2_cloudwatch_logs_role.name
 }
 
 # Security Group for EC2
-resource "aws_security_group" "ec2_security_group1" {
-  name        = "SG-EC2-Docker-Logging-Static"
+resource "aws_security_group" "ec2_security_group71" {
+  name        = "SG-EC2-Docker-Logging-Static1"
   description = "Allow SSH and HTTP inbound traffic"
   vpc_id      = data.aws_vpc.default.id
 
@@ -94,7 +94,7 @@ resource "aws_instance" "docker_ec2_instance" {
   key_name               = "personalawskey"  # Replace with your EC2 key pair
 
   iam_instance_profile   = aws_iam_instance_profile.ec2_cloudwatch_logs_profile1.name
-  security_groups        = [aws_security_group.ec2_security_group1.name]
+  security_groups        = [aws_security_group.ec2_security_group71.name]
 
   user_data = <<-EOF
     #!/bin/bash
