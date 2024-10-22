1c6f93dc2da3ee7c02b6052eade791051008d4b0
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-22 15:38:25 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,25 +63,26 @@ resource "aws_instance" "my_instance" {
   ami                    = "ami-084e237ffb23f8f97" # Update with your desired AMI
   instance_type         = "t2.micro"               # Adjust as necessary
   key_name              = "personalawskey"         # Update with your key pair
-  iam_instance_profile   = aws_iam_role.ec2_role.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name # Corrected reference
   security_groups       = [aws_security_group.ec2_sg22.name]
   
   user_data = <<-EOF
               #!/bin/bash
-              sudo su
               yum update -y
               yum install git -y
               amazon-linux-extras install docker -y
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ec2-user
-              pbcopy < ~/.ssh/id_rsa.pub
+              # Display the public key
               cat ~/.ssh/id_rsa.pub
+              # Clone your repository
               git clone https://github.com/your-repo/agri-pass-backend.git
               cd agri-pass-backend
               docker build -t agri-pass-backend-image .
               docker run -d --name my-app-container -p 80:80 agri-pass-backend-image
               yum install amazon-cloudwatch-agent -y
+              # Create the CloudWatch Agent configuration file
               cat << 'EOT' > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
               {
                 "logs": {
@@ -100,6 +101,5 @@ resource "aws_instance" "my_instance" {
               }
               EOT
               sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
-              docker run -d --name my-app-containernew --log-driver awslogs --log-opt awslogs-region=ap-southeast-2 --log-opt awslogs-group=docker-logs --log-opt awslogs-create-group=true -p 80:80 agri-pass-backend-image
               EOF
 }
