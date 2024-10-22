894c82a9f19d3735a25cf8e27b37fdd3a693cf52
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-22 16:16:38 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,23 +20,13 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "ec2-instance-profile-unique"  # Changed to a unique name
-  role = aws_iam_role.ec2_role.name
-}
-
 resource "aws_iam_role_policy_attachment" "cloudwatch_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
   role       = aws_iam_role.ec2_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-  role       = aws_iam_role.ec2_role.name
-}
-
 resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore" # Verify this ARN
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.ec2_role.name
 }
 
@@ -45,7 +35,12 @@ resource "aws_iam_role_policy_attachment" "ecs_ec2_role" {
   role       = aws_iam_role.ec2_role.name
 }
 
-resource "aws_security_group" "ec2_sg22" {
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_security_group" "ec2_sg" {
   name        = "ec2_security_group_unique"  # Changed to a unique name
   description = "Allow HTTP and SSH traffic"
   
@@ -69,7 +64,7 @@ resource "aws_instance" "my_instance" {
   instance_type         = "t2.micro"               # Adjust as necessary
   key_name              = "personalawskey"         # Update with your key pair
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
-  security_groups       = [aws_security_group.ec2_sg22.name]
+  security_groups       = [aws_security_group.ec2_sg.name]
   
   user_data = <<-EOF
               #!/bin/bash
@@ -81,9 +76,12 @@ resource "aws_instance" "my_instance" {
               usermod -aG docker ec2-user
               # Clone your repository
               git clone https://github.com/your-repo/agri-pass-backend.git
+              sleep 30
               cd agri-pass-backend
               docker build -t agri-pass-backend-image .
+              sleep 30
               docker run -d --name my-app-container -p 80:80 agri-pass-backend-image
+              sleep 30
               yum install amazon-cloudwatch-agent -y
               # Create the CloudWatch Agent configuration file
               cat << 'EOT' > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
