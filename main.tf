042e33fbfe9b399da2df0c455015b9acb8b12e57
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-23 16:21:33 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,9 +94,8 @@ resource "null_resource" "provision_ec2" {
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo mkdir -p /var/log/docker_logs",
-      "ssh-keyscan github.com >> ~/.ssh/known_hosts",
-      
-      "git clone https://github.com/agri-pass/agri-pass-backend.git /home/ec2-user/agri-pass-backend",
+      "ssh-keyscan github.com >> ~/.ssh/known_hosts",  
+      "git clone https://ysivasai99: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@github.com/agri-pass/agri-pass-backend.git /home/ec2-user/agri-pass-backend",
       "cd /home/ec2-user/agri-pass-backend",
       "sudo docker build -t myproject .",
       "sudo docker run -d -p 80:80 --log-driver=awslogs --log-opt awslogs-group=docker-logs --log-opt awslogs-stream=${aws_instance.ec2_instance.id} --log-opt awslogs-region=ap-southeast-2 -v /var/log/docker_logs:/var/log/app_logs myproject",
