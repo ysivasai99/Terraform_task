01947f25fbba6fefc03b7078fa9f0c87a4f50174
y sivasai <84569636+ysivasai99@users.noreply.github.com>
2024-10-23 10:54:22 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_full_access" {
 
 # Create an IAM instance profile
 resource "aws_iam_instance_profile" "ec2_instance_profileaas" {
-  name = "ec2-instance-profilegg"
+  name = "ec2_instance_profileaas"
   role = aws_iam_role.ec2_role.name
 }
 
